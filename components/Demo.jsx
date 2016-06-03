cb91de8ba4d885480d5cc0d87f4aabbc47742622
Corey <cjdunn11@gmail.com>
2016-06-03 14:07:24 -0700

    test

--- components/Demo.jsx
+++ components/Demo.jsx
@@ -1,10 +1,15 @@
-ReactDOM.render(
-	$('#textInput').bind('input propertychange', function() {
-				var text1 = $("#textInput").val();
+$('#textInput').bind('input propertychange', function() {
+			var text1 = $("#textInput").val();
 
-					marked(text1);
-	}),
 
-	document.getElementById('test')
 
-);
+				ReactDOM.render(
+
+						marked(text1),
+
+					document.getElementById('test')
+
+				);
+
+
+});
