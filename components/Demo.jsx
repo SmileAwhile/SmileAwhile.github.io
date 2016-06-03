7bdb4d52ec9b305be178e3a997766d41aeac7d60
Corey <cjdunn11@gmail.com>
2016-06-03 14:15:33 -0700

    test

--- components/Demo.jsx
+++ components/Demo.jsx
@@ -1,3 +1,15 @@
+var marked = require('marked');
+marked.setOptions({
+	renderer: new marked.Renderer(),
+	gfm: true,
+	tables: true,
+	breaks: false,
+	pedantic: false,
+	sanitize: true,
+	smartLists: true,
+	smartypants: false
+});
+
 $('#textInput').bind('input propertychange', function() {
 			var text1 = $("#textInput").val();
 
