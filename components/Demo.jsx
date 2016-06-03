e7d766cc6202e5d2f43d45c0726951d3b50a9bf5
Corey <cjdunn11@gmail.com>
2016-06-03 12:36:07 -0700

    test

---
+++ components/Demo.jsx
@@ -0,0 +1,28 @@
+var Demo = React.createClass({
+	render: function() {
+		return (
+			<div className="container">
+			<h1> heading</h1>
+			<h2>List</h2>
+			<ul>
+			<li>something</li>
+
+			<li>something</li>
+
+			<li>something</li>
+
+
+			</ul>
+
+
+
+
+
+
+
+			</div>
+			
+
+			);
+	}
+});
