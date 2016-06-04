2d012c3e97d9b77fda26d6f597a02d2634f0e755
Corey <cjdunn11@gmail.com>
2016-06-03 21:34:06 -0700

    update

--- markdown/js/module1.js
+++ markdown/js/module1.js
@@ -33,3 +33,12 @@ $('#textInput').bind('input propertychange', function() {
 				document.getElementById('test')
 			);
 });
+$('#textInput').val("Heading\n=======\n\nSub-heading\n-----------\n\n### Another deeper heading\n\nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**,\n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n* apples\n* oranges\n* pears\n\nNumbered list:\n\n1. apples\n2. oranges\n3. pears\n\nThe rain---not the reign---in\nSpain.\n\n*[Corey Dunn](http://smileawhile.github.io)*");
+
+$(document).ready(function() {
+	var text2 = marked($('#textInput').val());
+	ReactDOM.render(
+		<MarkdownPreview text={text2} />,
+		document.getElementById('test')
+	);
+});
