34f809d1421f3aa5784fe387c19f2177e93432f4
Corey <cjdunn11@gmail.com>
2016-06-09 12:18:13 -0700

    more fixes

--- markdown/js/MarkdownPrev.js
+++ markdown/js/MarkdownPrev.js
@@ -38,6 +38,7 @@ export default class MarkdownPrev extends React.Component {
 			<div class="container-fluid">
 				<link rel="stylesheet" href="markdown/css/style.css" />
 				<a href="#/" ><button class="btn btn-primary">HOME</button></a>
+				<span class="signed">Built with React and Sass by Corey Dunn in 2016</span>
 				<div>
 					<textArea id="textInput" className="col-xs-4" rows="40" cols="50"
 					defaultValue={"Heading\n=======\n\nSub-heading\n-----------\n\n### Another deeper heading\n\nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**,\n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n* apples\n* oranges\n* pears\n\nNumbered list:\n\n1. apples\n2. oranges\n3. pears\n\nThe rain---not the reign---in\nSpain.\n\n*[Corey Dunn](http://smileawhile.github.io)*"}
